7b2a921e58fe1fbfacc91ffff8f2b6a66199a6be
Geert Fannes <geert.fannes@auro-technologies.com>
2017-11-05 22:03:14 +0100

    Files can be added

--- src/cook/model/Recipe.hpp
+++ src/cook/model/Recipe.hpp
@@ -1,9 +1,25 @@
 #ifndef HEADER_cook_model_Recipe_hpp_ALREADY_INCLUDED
 #define HEADER_cook_model_Recipe_hpp_ALREADY_INCLUDED
 
+#include "gubg/file/System.hpp"
+#include "gubg/macro/capture.hpp"
+
 namespace cook { namespace model { 
 
     class Book;
+
+    struct File
+    {
+        std::filesystem::path dir;
+        std::filesystem::path path;
+        std::string language;
+
+        void stream(std::ostream &os) const
+        {
+            os << C(path)C(dir)C(language);
+        }
+    };
+    using FilePerPath = std::map<std::filesystem::path, File>;
 
     class Recipe
     {
@@ -20,13 +36,36 @@ namespace cook { namespace model {
             MSS_END();
         }
 
-        void add(const std::string &dir, const std::string &pattern)
+        void add(std::string p_dir, const std::string &pattern)
         {
+            if (p_dir.empty())
+                p_dir = ".";
+
+            std::filesystem::path dir = p_dir;
+            if (dir.is_relative())
+                dir = wd_ / dir;
+
+            auto add_file = [&](const std::filesystem::path &fp)
+            {
+                auto &file = file_per_path_[fp];
+                file.dir = dir;
+                file.path = fp;
+                const auto ext = fp.extension();
+                if (false) {}
+                else if (ext == ".c" || ext == ".h") {file.language = "c";}
+                else if (ext == ".cpp" || ext == ".hpp") {file.language = "c++";}
+                else if (ext == ".asm") {file.language = "asm";}
+            };
+            gubg::file::each_glob(pattern, add_file, dir);
         }
 
         void stream(std::ostream &os) const
         {
-            os << name_ << ", type: " << type_ << ", working directory: " << wd_ << std::endl;
+            os << name_ << ", type: " << type_ << ", working directory: " << wd_ << ", nr files: " << file_per_path_.size() << std::endl;
+            for (const auto &p: file_per_path_)
+            {
+                os << "\t"; p.second.stream(os); os << std::endl;
+            }
         }
 
     private:
@@ -34,7 +73,8 @@ namespace cook { namespace model {
 
         std::string name_;
         std::string type_;
-        std::string wd_;
+        std::filesystem::path wd_;
+        FilePerPath file_per_path_;
     };
 
 } } 
