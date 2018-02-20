d14028c2bd88fc8fd207cfe39caa80f7b540f8bc
Thomas Fannes <thomas.fannes@auro-technologies.com>
2018-02-20 09:59:31 +0100

    [refactor](message has no default constructor)

--- lib/src/cook/Message.hpp
+++ lib/src/cook/Message.hpp
@@ -8,12 +8,11 @@ namespace cook {
 
 enum class MessageType
 {
-    Undefined = 0x00,
-    Success = 0x01,
-    Warning = 0x02,
-    Info = 0x04,
-    Error = 0x08,
-    InternalError = 0x010,
+    Success = 0x00,
+    Warning = 0x01,
+    Info = 0x02,
+    Error = 0x04,
+    InternalError = 0x008,
 };
 
 inline MessageType operator&(MessageType lhs, MessageType rhs)
@@ -45,12 +44,9 @@ struct Message
 {
     using Reporter = std::function<void (std::ostream &)>;
 
-    Message()
-        : type(MessageType::Undefined) {}
-
-    explicit Message(MessageType type, Reporter reporter = Reporter())
-        : type(type),
-          reporter(reporter)
+    explicit Message(MessageType message_type, Reporter report_functor = Reporter())
+        : type(message_type),
+          reporter(report_functor)
     {
     }
 
@@ -62,7 +58,6 @@ inline Message make_error_message(Message::Reporter reporter = Message::Reporter
 inline Message make_internal_error_message(Message::Reporter reporter = Message::Reporter())    { return Message(MessageType::InternalError,    reporter); }
 inline Message make_warning_message(Message::Reporter reporter = Message::Reporter())           { return Message(MessageType::Warning,          reporter); }
 inline Message make_info_message(Message::Reporter reporter = Message::Reporter())              { return Message(MessageType::Info,             reporter); }
-inline Message make_success()                                                                   { return Message(MessageType::Success); }
 
 }
 
