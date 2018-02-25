e28492a7a6894a1f2fa1fefd5dfb464d256d4475
Thomas Fannes <thomas.fannes@auro-technologies.com>
2018-02-25 12:23:27 +0100

    [dev](default merge for overwrite)

---
+++ lib/src/cook/toolchain/CompileLinkArchive.cpp
@@ -0,0 +1,6 @@
+#include "CompileLinkArchive.hpp"
+
+CompileLinkArchive::CompileLinkArchive()
+{
+
+}
---
+++ lib/src/cook/toolchain/CompileLinkArchive.hpp
@@ -0,0 +1,11 @@
+#ifndef COMPILELINKARCHIVE_HPP
+#define COMPILELINKARCHIVE_HPP
+
+
+class CompileLinkArchive
+{
+public:
+    CompileLinkArchive();
+};
+
+#endif // COMPILELINKARCHIVE_HPP
