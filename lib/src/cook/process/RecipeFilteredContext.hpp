5109026ae0437efbabd9144b6d3cc3dc5f010e4b
Thomas Fannes <thomas.fannes@auro-technologies.com>
2018-03-01 21:09:51 +0100

    [dev](filtered context recipe based for souschef)

--- lib/src/cook/process/RecipeFilteredContext.hpp
+++ lib/src/cook/process/RecipeFilteredContext.hpp
@@ -1,16 +1,18 @@
 #ifndef HEADER_cook_process_RecipeContext_hpp_ALREADY_INCLUDED
 #define HEADER_cook_process_RecipeContext_hpp_ALREADY_INCLUDED
 
+#include "cook/process/RecipeFilteredGraph.hpp"
 #include "cook/model/Recipe.hpp"
 #include <list>
 
 namespace cook { namespace process { namespace build {
 
-struct LocalContext
+struct RecipeFilteredContext
 {
     model::Recipe * recipe;
     std::list<const model::Recipe *> topological_order;
     std::list<const model::Snapshot *> dependent_post_shots;
+    RecipeFilteredGraph * graph;
 };
 
 } } }
