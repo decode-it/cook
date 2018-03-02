fddcd6e03e4331216890d5306d00d7eca03d2332
Thomas Fannes <thomas.fannes@auro-technologies.com>
2018-03-02 08:26:02 +0100

    [refactor](algo is template function)

--- lib/src/cook/algo/TopologicalOrder.hpp
+++ lib/src/cook/algo/TopologicalOrder.hpp
@@ -3,8 +3,33 @@
 
 #include "cook/model/Recipe.hpp"
 #include "cook/Result.hpp"
+#include "boost/graph/topological_sort.hpp"
 
 namespace cook { namespace algo {
+
+
+// topological dependencies have an element with zero dependencies at index 0
+template <typename DependencyGraph, typename OutIterator>
+Result make_TopologicalOrder(const DependencyGraph & dependency_graph, OutIterator out_iterator)
+{
+    MSS_BEGIN(Result);
+    using Vertex = typename boost::graph_traits<g>::vertex_descriptor;
+
+    std::vector<Vertex> vertices(boost::num_vertices(dependency_graph));
+
+    try
+    {
+        boost::topological_sort(dependency_graph, vertices.begin());
+    }
+    catch(boost::not_a_dag)
+    {
+        MSG_MSS(false, Error, "Cyclic dependency detected");
+    }
+
+    std::transform(RANGE(vertices), out_iterator, [&](Vertex v) { return dependency_graph[v]; });
+
+    MSS_END();
+}
 
 Result make_TopologicalOrder(const std::list<model::Recipe *> & roots, std::list<model::Recipe*> & make_TopologicalOrder);
 Result make_TopologicalOrder(model::Recipe * root, std::list<model::Recipe*> & make_TopologicalOrder);
