e5c27fe2338ad3ea765d8a4188971511d2224de3
Thomas Fannes <thomas.fannes@auro-technologies.com>
2018-03-04 16:39:27 +0100

    [dev](include path with no language)

--- lib/src/cook/process/souschef/IncludePathSetter.cpp
+++ lib/src/cook/process/souschef/IncludePathSetter.cpp
@@ -7,21 +7,19 @@ namespace cook { namespace process { namespace souschef {
 IncludePathSetter::IncludePathSetter(Language language)
     : language_(language)
 {
-
-}
+    }
 
 Result IncludePathSetter::process(model::Recipe & recipe, RecipeFilteredGraph & /*file_command_graph*/, const Context & /*context*/) const
 {
     MSS_BEGIN(Result);
 
     auto & files = recipe.files();
-    auto it = files.find(LanguageTypePair(language_, Type::Header));
 
-    if (it == files.end())
-        MSS_RETURN_OK();
-
-    for(const ingredient::File & file : it->second)
+    for(const auto & file : files.range(LanguageTypePair(language_, Type::Header)))
         MSS(add_derived_path(LanguageTypePair(language_, Type::IncludePath), file, recipe));
+
+    for(const auto & file : files.range(LanguageTypePair(Language::Undefined, Type::IncludePath)))
+        MSS(files.insert_or_merge(LanguageTypePair(language_, Type::IncludePath), file));
 
     MSS_END();
 }
