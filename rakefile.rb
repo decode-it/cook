138ed6c2ae436611e7cf07a0896144a7bbf06200
Geert Fannes <geert.fannes@auro-technologies.com>
2018-05-14 17:23:39 +0200

    [fix] of c++.std

--- rakefile.rb
+++ rakefile.rb
@@ -150,7 +150,7 @@ namespace :b1 do
         sh("./unit_tests.exe -a -d yes")
     end
 
-    toolchain_options = {"c++-standard" => 17, "release" => nil}
+    toolchain_options = {"c++.std" => 17, "release" => nil}
     case GUBG::os
     when :macos     then toolchain_options["arch"] = "x64"
     when :windows   then toolchain_options["arch"] = "x86"
