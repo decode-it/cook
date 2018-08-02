fc52577b085160994ba8b212ca7adec32afcd9d6
Geert Fannes <geert.fannes@auro-technologies.com>
2018-08-02 10:27:15 +0200

    [test] Added scenario to demo how an app can link against a static lib with self-chosen name
    * TODO: cmake does not handle the arch rename yet

---
+++ scenario/app_arch_func.rb
@@ -0,0 +1,26 @@
+require("gubg/catch")
+include GUBG::Catch
+
+test_case("app_arch_func") do
+    cook_fn = File.expand_path("cook.exe")
+    should = nil
+    cmd = [cook_fn]
+    # cmd << "-v 3"
+    section("positive") do
+        should = :pass
+        cmd << "-f scenario/app_arch_func/recipes.chai"
+        cmd << "-o scenario/app_arch_func"
+        section("ninja") do
+            cmd << "-g ninja"
+        end
+        section("cmake") do
+            cmd << "-g cmake"
+        end
+    end
+    sh(cmd.flatten*' ') do |ok, res|
+        case should
+        when :pass then must(ok)
+        when :fail then must(!ok)
+        else raise("Unknown should #{should}") end
+    end
+end
---
+++ scenario/app_arch_func/func/a.hpp
@@ -0,0 +1,8 @@
+#ifndef HEADER_func_a_hpp_ALREADY_INCLUDED
+#define HEADER_func_a_hpp_ALREADY_INCLUDED
+
+
+int f();
+
+
+#endif
