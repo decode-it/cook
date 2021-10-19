da9644707104964a59f69c14123d2c1a4230b55e
Geert Fannes <geert.fannes@gmail.com>
2021-10-19 09:07:52 +0200

    Actual bundle builds with Xcode are now only performed for macos

--- scenario/bundle.rb
+++ scenario/bundle.rb
@@ -6,28 +6,28 @@ test_case("bundle") do
 
 	name = nil
 
-    # section("app") do
-    # 	name = "app"
-    # end
-    # section("app_icns") do
-    # 	name = "app_icns"
-    # end
-    # section("app_plist") do
-    # 	name = "app_plist"
-    # end
+    section("app") do
+    	name = "app"
+    end
+    section("app_icns") do
+    	name = "app_icns"
+    end
+    section("app_plist") do
+    	name = "app_plist"
+    end
     section("app_params") do
     	name = "app_params"
     end
-    # section("app_resource") do
-    # 	name = "app_resource"
-    # end
+    section("app_resource") do
+    	name = "app_resource"
+    end
 
-    # section("plugin") do
-    # 	name = "plugin"
-    # end
-    # section("plugin_ext") do
-    # 	name = "plugin_ext"
-    # end
+    section("plugin") do
+    	name = "plugin"
+    end
+    section("plugin_ext") do
+    	name = "plugin_ext"
+    end
 
     Dir.chdir("scenario/bundle") do
     	FileUtils.rm_f("CMakeLists.txt")
@@ -37,10 +37,12 @@ test_case("bundle") do
     	cmake_build = "cmake_build-#{name}"
     	FileUtils.rm_rf(cmake_build)
 
-    	sh("cmake -S . -B #{cmake_build} -G Xcode")
-    	case :xcode
-    	when :xcode then sh("xcodebuild -project #{cmake_build}/*.xcodeproj")
-    	when :cmake then sh("cmake --build #{cmake_build}")
-    	end
-    end
+        if GUBG.os == :macos
+           sh("cmake -S . -B #{cmake_build} -G Xcode")
+           case :xcode
+           when :xcode then sh("xcodebuild -project #{cmake_build}/*.xcodeproj")
+           when :cmake then sh("cmake --build #{cmake_build}")
+           end
+       end
+   end
 end
